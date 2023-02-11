09259800834abfe6a2f33d585f20caf528b00942
alishashingala188 <alishashingala123@gmail.com>
2023-02-11 17:18:33 +0530

    LoginPage

--- pcs/src/Login.js
+++ pcs/src/Login.js
@@ -3,20 +3,30 @@ import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@mui/mat
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
+import background from './images/25.jpg';
+
 const Login=()=>{
 
     const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
     const avatarStyle={backgroundColor:'#1bbd7e'}
     const btnstyle={margin:'8px 0'}
+    const main={backgroundImage:`url(${background})`, backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    width: '100vw',
+    height: '100vh',
+backgroundColor:'#03A9F4'}
+
     return(
-        <Grid>
-            <Paper elevation={10} style={paperStyle}>
+        <div >
+        <Grid  >
+            <Paper elevation={10} style={paperStyle}   >
                 <Grid align='center'>
                      <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
                     <h2>Sign In</h2>
                 </Grid>
-                <TextField label='Username' placeholder='Enter username'  sx={{mb:3}} fullWidth required/ >
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <TextField label='Username' placeholder='Enter username'  sx={{mb:2}} fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' sx={{mb:2}}  type='password' fullWidth required/>
                 <FormControlLabel
                     control={
                     <Checkbox
@@ -27,18 +37,11 @@ const Login=()=>{
                     label="Remember me"
                  />
                 <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
-                <Typography >
-                     <Link href="#" >
-                        Forgot password ?
-                </Link>
-                </Typography>
-                <Typography > Do you have an account ?
-                     <Link href="#" >
-                        Sign Up 
-                </Link>
-                </Typography>
+                
             </Paper>
+          
         </Grid>
+        </div>
     )
 }
 
