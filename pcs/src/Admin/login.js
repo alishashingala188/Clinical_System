9bd21e8b1d3eed4710a36b565481b4b3d6ab3eee
alishashingala188 <alishashingala123@gmail.com>
2023-02-11 14:36:53 +0530

    LoginPageCreated

---
+++ pcs/src/Admin/login.js
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import LoginImages from '../images/back.jpg'
+import { width } from '@mui/system';
+
+const theme = createTheme();
+const commonStyle={
+    border:2,
+    borderColor:'text.primary',
+    width:'25rem',
+
+};
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
+  };
+
+  return (
+    <div>  
+     <img src={LoginImages} width="1150" height="500" />
+          <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="sx">
+     
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: -60,
+           marginLeft:80,
+            alignItems: 'center',
+            
+          }}
+        >
+             
+          <Avatar sx={{ m: 1, ml : 20,bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5" sx={{ ml : 18}} >
+           Sign In
+          </Typography>
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3,...commonStyle }}>
+            <Grid container spacing={2}>
+              
+             
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            
+          </Box>
+        </Box>
+      
+      </Container>
+    </ThemeProvider>
+    </div>
+  );
+}
