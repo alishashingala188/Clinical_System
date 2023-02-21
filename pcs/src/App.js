866ba318258568e99e6d32f22c5ae1f76411d221
alishashingala188 <alishashingala123@gmail.com>
2023-02-21 12:58:38 +0530

    changess

--- pcs/src/App.js
+++ pcs/src/App.js
@@ -1,20 +1,24 @@
-
-  import "./App.css";
-  import AppFooter from "./Components/AppFooter";
-  import AppHeader from "./Components/AppHeader";
-  import PageContent from "./Components/PageContent";
-  import SideMenu from "./Components/SideMenu";
-  
-  function App() {
-    return (
-      <div className="App">
-        <AppHeader />
-        <div className="SideMenuAndPageContent">
-          <SideMenu></SideMenu>
-          <PageContent></PageContent>
-        </div>
-        <AppFooter />
-      </div>
-    );
-  }
-  export default App;
+import React from 'react'
+import Login from './Login'
+import Main from './Main'
+import AppHeader from './Components/AppHeader/'
+import PageContent from './Components/PageContent/'
+import SideMenu from './Components/SideMenu/'
+import AppFooter from './Components/AppFooter/'
+import './App.css'
+const App = () => {
+  return (
+    <>
+    <div className='App'>
+    <AppHeader />
+    <div className='SideMenuAndPageContent'>
+      <SideMenu />
+      <PageContent />
+    </div>
+   <AppFooter />
+   </div>
+    </>
+    
+  )
+}
+export default App;
